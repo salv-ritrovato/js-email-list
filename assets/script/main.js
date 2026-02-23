8ef3b3f51a20e70057cb548842afdb51cd9879aa
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-23 14:02:31 +0100

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,9 @@
+// API
+const booleanMails = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+// DOM MANIPULATION
+const btnRefresh = document.getElementById('refresh')
+const mailList = document.getElementById('mail-list')
+
+// CALL TO API + FUNCTION
+function
