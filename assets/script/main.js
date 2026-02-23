9e53fb7d6a0c7cc5e1308520acbc5614a01018ab
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-23 14:26:06 +0100

    task completed

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,4 +6,26 @@ const btnRefresh = document.getElementById('refresh')
 const mailList = document.getElementById('mail-list')
 
 // CALL TO API + FUNCTION
-function
+// Creo una funzione, genero un ciclo for ed inserisco all'interno di esso il fetch
+// In questo modo il ciclo effettua 10 chiamate API generando 
+function refreshMail() {
+    for (let i = 0; i < 10; i++) {
+        fetch(booleanMails)
+            .then(res => res.json())
+            .then(mail => {
+                console.log(mail);
+                mailList.innerHTML += `<li>${mail.response}</li>`
+            })
+    }
+}
+
+// RICHIAMO la funzione per stampare in pagina i 10 risultati
+refreshMail()
+
+// EVENT LISTENER per il bottone
+// Servirà a refreshare la lista quando il listener intercetta il click dell'utente
+btnRefresh.addEventListener('click', function(){
+    // Inserisco questa linea di codice per ""svuotare"" la lista al click del bottone, prevenendo l'accumulo di emails
+    mailList.innerHTML = '';
+   refreshMail() 
+})
